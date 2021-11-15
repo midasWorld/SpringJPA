1efe2c6c009c8d35c9c79629b5d87d2768c60b24
midasWorld <midas117@knou.ac.kr>
2021-11-16 06:27:16 +0900

    feat : spring-security 추가 & security-config 클래스 생성

---
+++ src/main/java/com/midas/springjpa/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.midas.springjpa.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/posts/**").permitAll();
+    }
+}
