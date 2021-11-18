9f6bdbbd1a04e4d96ac5f7c57d0d5244a7f9d6cb
midasWorld <midas117@knou.ac.kr>
2021-11-18 20:01:19 +0900

    feat : 다국어 처리 설정 및 테스트 완료

--- src/main/java/com/midas/springjpa/SpringJpaApplication.java
+++ src/main/java/com/midas/springjpa/SpringJpaApplication.java
@@ -2,7 +2,12 @@ package com.midas.springjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringJpaApplication {
@@ -11,4 +16,10 @@ public class SpringJpaApplication {
         SpringApplication.run(SpringJpaApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
