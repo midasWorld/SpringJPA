43b1c18adcfada5223034989e1228b3f7da36dd4
midasworld <midas117@knou.ac.kr>
2021-12-06 22:24:06 +0900

    reset : 다시 공부를 위해 리셋!

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user (email, name, password, ssn) values ('user1@gmail.com', 'user1', '1234', '111-1111');
-insert into user (email, name, password, ssn) values ('user2@gmail.com', 'user2', '1234', '111-1111');
-insert into user (email, name, password, ssn) values ('user3@gmail.com', 'user3', '1234', '111-1111');
-insert into user (email, name, password, ssn) values ('user4@gmail.com', 'user4', '1234', '111-1111');
