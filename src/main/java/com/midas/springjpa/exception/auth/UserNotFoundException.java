441c8f8dbfcc5ba5398d8dc96f649e4366733c39
midasworld <midas117@knou.ac.kr>
2022-02-04 11:47:41 +0900

    reset : 다시 공부를 위해 리셋!

--- src/main/java/com/midas/springjpa/exception/auth/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.midas.springjpa.exception.auth;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/midas/springjpa/web/dto/auth/SigninRequest.java
+++
@@ -1,9 +0,0 @@
-package com.midas.springjpa.web.dto.auth;
-
-import lombok.Data;
-
-@Data
-public class SigninRequest {
-    private String email;
-    private String password;
-}
