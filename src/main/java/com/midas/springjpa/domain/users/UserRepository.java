34651ec4a1ab829dce8433f6a6cf93f950997619
midasworld <midas117@knou.ac.kr>
2022-02-09 19:40:11 +0900

    reset : QueryDSL 공부를 위해 모두 리셋

--- src/main/java/com/midas/springjpa/domain/users/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.midas.springjpa.domain.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
